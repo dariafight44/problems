4f66d5d443ce92109dbec4ce720c7ca76b834092
EgorIK55 <mat.matveyaka@gmail.com>
2024-03-10 12:57:47 +0700

    gh

--- base_fales/__init__.py
+++ base_fales/__init__.py
@@ -16,6 +16,24 @@ def create_app():
     def display_words2():
         return render_template("oldwords.html")
     
+    @app.route("/words/ava")
+    def display_ava():
+        return render_template("ava.html")
+    
+    @app.route("/words/vibe")
+    def display_vibe():
+        return render_template("vibe.html")
+    
+    @app.route("/wordsold/sovparshiv")
+    def display_sovparshiv():
+        return render_template("sovparshiv.html")
+    
+    @app.route("/wordsold/shnurky")
+    def display_shnurky():
+        return render_template("shnurky.html")
+    
+
+    
 
 
 
