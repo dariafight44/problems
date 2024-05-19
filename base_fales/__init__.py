6df361721e8e4b0dc4e29fd5340d531d01908a55
EgorIK55 <mat.matveyaka@gmail.com>
2024-05-19 13:10:37 +0700

    fdghjk

--- base_fales/__init__.py
+++ base_fales/__init__.py
@@ -1,20 +1,36 @@
 from flask import Flask
 from flask import render_template
 
+import requests
+import json
+
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    result = json.loads(requests.get("http://127.0.0.1:8080/slang/dinosaurus").text)
+    print(result)
+
+    @app.route("/test")
+    def test():
+        result = json.loads(requests.get("http://127.0.0.1:8080/slang/dinosaurus").text)
+        return render_template("test.html", testResult = result)
+
+
 
     @app.route("/")
     def display_main_page():
         return render_template("index.html")
     @app.route("/words")
     def display_words1():
-        return render_template("words.html")
+        result = json.loads(requests.get("http://127.0.0.1:8080/slang/today").text)
+        print(result)
+        return render_template("words.html", slangTResult = result)
     
     @app.route("/wordsold")
     def display_words2():
-        return render_template("oldwords.html")
+        result = json.loads(requests.get("http://127.0.0.1:8080/slang/dinosaurus").text)
+        return render_template("oldwords.html", slangDREsult = result)
     
     @app.route("/words/ava")
     def display_ava():
---
+++ base_fales/model/website_types.py
@@ -0,0 +1,33 @@
+class SlangToday:
+    id = 0
+    name  = ""
+    description = ""
+
+    def __init__(self, id, name, description):
+        self.description = description
+        self.id = id
+        self.name = name
+
+
+
+
+
+class SlangDinosaurus:
+    id = 0
+    name  = ""
+    description = ""
+
+    def __init__(self, id, name, description):
+        self.name = name
+        self.id = id
+        self.description = description
+
+
+
+
+
+
+
+
+
+
